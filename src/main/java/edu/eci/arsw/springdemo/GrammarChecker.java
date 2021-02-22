fdc1294ea8b25b094ce1ff1c0c616ef97c7e230f
davinchicoronado <david.coronado@mail.escuelaing.edu.co>
2021-02-22 14:44:29 -0500

    addAnnotations

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -4,29 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+@Service
 public class GrammarChecker {
 
+        @Autowired
 	SpellChecker sc;
 
 	String x;
-        
-        
-	public SpellChecker getSpellChecker() {
-		return sc;
-	}
 
-	public void setSpellChecker(SpellChecker sc) {
-		this.sc = sc;
-	}
+    public SpellChecker getSc() {
+        return sc;
+    }
 
+    public void setSc(SpellChecker sc) {
+        this.sc = sc;
+    }
 
-	public String check(String text){
+    public String check(String text){
 		
 		StringBuffer sb=new StringBuffer();
 		sb.append("Spell checking output:"+sc.checkSpell(text));
 		sb.append("Plagiarism checking output: Not available yet");
-		
-		
 		return sb.toString();
 		
 	}
